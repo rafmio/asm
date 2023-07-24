ab949c648b5535cf259221abef6e413edd4a92cc
Rafael Idrisov <rafael.idirisov@gmail.com>
2023-07-24 16:28:57 +0300

    Work PC 24.07.2023 16:28

---
+++ 005-arithmetic-exers.s
@@ -0,0 +1,22 @@
+.globl _start
+.section .text
+_start:
+	movq $300, %rax
+	movq $300, %rbx
+	movq $300, %rcx
+	movq $300, %rdx
+	movq $300, %rsi
+	movq $300, %rdi
+	movq $300, %rbp
+	movq $300, %rsp
+	movq $300, %r8
+	movq $300, %r9
+	movq $300, %r10
+	movq $300, %r11
+	movq $300, %r12
+	movq $300, %r13
+	movq $300, %r14
+	movq $300, %r15 
+	movq $8,   %r8
+	divq %r8
+	syscall
