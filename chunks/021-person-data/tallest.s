e162830d28f0cb188e2ff4713c409c295644d1fd
Rafael Idrisov <rafael.idrisov@gmail.com>
2023-08-26 16:56:05 +0300

    Home PC 26.08.2023 16:56

---
+++ chunks/021-person-data/tallest.s
@@ -0,0 +1,39 @@
+.globl _start
+.section .text
+_start:
+	### Initialize Registers ###
+	leaq people, %rbx			# pointer to first record
+	movq numpeople, %rcx			# record count
+	movq $0, %rdi				# tallest value found
+	
+	### Check preconditions ###
+
+	cmpq $0, %rcx
+	je finish
+
+mainloop:
+	# %rbx is the pointer to the whole struct
+	# this instruction grabs the height field
+	# and stores it in %rax
+	
+	movq HEIGHT_OFFSET(%rbx), %rax	# if it is less than of equal to our current
+	# tallest, go to the next one
+	
+	cmpq %rdi, %rax
+	jbe endloop
+
+	mov %rax, %rdi				# copy this value as the tallest value
+
+endloop:
+	# move %rbx to point to the next record	
+	
+	addq $PERSON_RECORD_SIZE, %rbx
+
+	loopq mainloop				# decrement %rcx and do it again
+
+	### Finesh it off ###
+
+finish:
+	movq $60, %rax
+	syscall
+
