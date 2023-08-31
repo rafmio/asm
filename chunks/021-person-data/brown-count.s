c499830135eca7dfbd7c8b37954d221ac2b0030b
Rafael Idrisov <rafael.idrisov@gmail.com>
2023-08-31 22:21:00 +0300

    Home PC 31.08.2023 22:20

---
+++ chunks/021-person-data/brown-count.s
@@ -0,0 +1,43 @@
+.globl _start
+.section .text
+_start:
+	### Initialize registers ###
+	
+	# Pointer to first record 
+	leaq people, %rbx
+
+	# Total record count
+	movq numpeople, %rcx
+
+	# Brown-hair count
+	movq $0, %rdi
+
+	### Check preconditions ###
+
+	# if there are no record, finish
+	cmpq $0, %rcx
+	je finish
+
+
+	### MAIN LOOP ###
+mainloop:
+	# is the hair color brown (2)?
+	cmpq $2, HAIR_OFFSET(%rbx)
+
+	# No? Go to the next record
+	jne endloop
+
+	# Yes? Increment the count
+	incq %rdi
+
+endloop:	
+	addq $PERSON_RECORD_SIZE, %rbx
+	loop mainloop
+
+finish:
+	movq $60, %rax
+	syscall
+
+
+# $ as brown-count.s -o brown-count.o
+# $ ls person-data.o brown-count.o - brown-cownt
